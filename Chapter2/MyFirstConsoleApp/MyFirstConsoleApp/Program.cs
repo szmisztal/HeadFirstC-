2f4d5cd47c8e471fffd548289b7c11b0b2a9f00c
szmisztal <sz.misztal@gmail.com>
2024-07-22 11:07:24 +0200

    add Chapter2 dir and exercises with loops, if etc

---
+++ Chapter2/MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TryAnIf();
+            TrySomeLoops();
+            TryAnIfElse();
+        }
+
+        private static void TryAnIf()
+        {
+            int someValue = 4;
+            string name = "Bobbo Junior";
+
+            if ((someValue == 3) && (name == "Jasiek"))
+            {
+                Console.WriteLine("x is 3 and name is Jasiek");
+            }
+
+            Console.WriteLine("This line is always printing");
+        }
+
+        private static void TryAnIfElse()
+        {
+            int x = 5;
+
+            if (x == 10)
+            {
+                Console.WriteLine("x has to be equal 10");
+            }
+            else
+            {
+                Console.WriteLine("x is not equal 10");
+            }
+        }
+
+        private static void TrySomeLoops()
+        {
+            int count = 0;
+
+            while (count < 10)
+            {
+                count = count + 1;
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                count = count - 1;
+            }
+
+            Console.WriteLine("Result is " + count);
+
+        }
+    }
+}
