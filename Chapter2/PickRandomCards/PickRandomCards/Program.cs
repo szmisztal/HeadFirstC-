20e81d43aae273a62e253bd173a0cb7912bc92e3
szmisztal <sz.misztal@gmail.com>
2024-07-26 12:11:17 +0200

    add Main method to project

--- Chapter2/PickRandomCards/PickRandomCards/Program.cs
+++ Chapter2/PickRandomCards/PickRandomCards/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PickRandomCards
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wpisz liczbę kart do wygenerowania: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.Write("Nieprawidłowe dane, wpisz liczbę");
+            }
+
+
+        }
+    }
+}
