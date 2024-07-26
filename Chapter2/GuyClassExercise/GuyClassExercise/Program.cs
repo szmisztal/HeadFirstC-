a87a6b0d32aeffe13e3b713e7c0b2adb919e15b4
szmisztal <sz.misztal@gmail.com>
2024-07-26 19:37:53 +0200

    add exercise with classes

---
+++ Chapter2/GuyClassExercise/GuyClassExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace GuyClassExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guy jacek = new Guy() { Name = "Jacek", Cash = 50 };
+            Guy bartek = new Guy() { Name = "Bartek", Cash = 100 };
+
+            while (true)
+            {
+                jacek.WriteMyInfo();
+                bartek.WriteMyInfo();
+                Console.Write("Podaj kwotę: ");
+                string howMuch = Console.ReadLine();
+                if (howMuch == "") return;
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    Console.Write("Pieniądze ma przekazać: ");
+                    string whichGuy = Console.ReadLine();
+                    if (whichGuy == "Jacek")
+                    {
+                        int cashGiven = jacek.GiveCash(amount);
+                        bartek.ReceiveCash(cashGiven);
+                    }
+                    else if (whichGuy == "Bartek")
+                    {
+                        int cashGiven = bartek.GiveCash(amount);
+                        jacek.ReceiveCash(cashGiven);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wpisz: Jacek lub Bartek");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wpisz kwotę (lub pusty wiersz, aby zakończyć)");
+                }
+            }
+        }
+    }
+}
