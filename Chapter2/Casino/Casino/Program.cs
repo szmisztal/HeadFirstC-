e6f74a29e5cb807af1b4a20887324887b7a19a2a
szmisztal <sz.misztal@gmail.com>
2024-07-28 10:10:41 +0200

    add Casino project

---
+++ Chapter2/Casino/Casino/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Casino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(); 
+            Double odds = 0.75;
+            Player player = new Player() { Name = "Player", Cash = 100 };
+
+            while (player.Cash > 0)
+            {
+                player.WriteMyInfo();
+                Console.WriteLine("Podaj stawkę: ");
+                string howMuch = Console.ReadLine();
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    int pot = amount * 2;
+                    double result = random.NextDouble();
+                    if (result > odds)
+                    {
+                        player.Cash += pot;
+                        Console.WriteLine("Wygrałeś !");
+                    }
+                    else
+                    {
+                        player.Cash -= pot;
+                        Console.WriteLine("Przegrałeś...");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Musisz podać kwotę");
+                }
+            }
+        }
+    }
+}
